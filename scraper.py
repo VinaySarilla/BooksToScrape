fe0c761cec2b1204c9be6c0954d1a2e1069db262
ojas <ojas91196@gmail.com>
2020-12-04 19:17:35 +0530

    Done Scraping & DB

--- scraper.py
+++ scraper.py
@@ -1,69 +1,83 @@
 import requests
-import csv
 from bs4 import BeautifulSoup
 import pymysql
-import db
+import fb
+import time
 
-def trade_spider(maxpages):
+global t0
+global t1
+
+def getNoOfPages():
+    url = 'https://books.toscrape.com'
+    sourceCode = requests.get(url)
+    plainText = sourceCode.text
+    soup = BeautifulSoup(plainText, 'html.parser')
+    noOfPages = soup.find('li', {'class':'current'}).string.strip()
+    noOfPages = int(noOfPages[-2:])
+    return noOfPages
+
+def tradeSpider():
     page = 1
-    while page <= maxpages:
-        url = 'http://books.toscrape.com/catalogue/page-'+str(page)+'.html'
-        source_code = requests.get(url)
-        plain_text = source_code.text
-        soup = BeautifulSoup(plain_text,'html.parser')
+    noOfPages=getNoOfPages()
 
-        for link in soup.findAll('article', {'class':'product_pod'}):
-            href = 'http://books.toscrape.com/catalogue/' + link.h3.a.get('href')
-            title = link.h3.a.get('title')
-            rating = link.p.get('class')
-            print(href)
-
-            each_book_detail(href)
-        page+=1
-
-def each_book_detail(book_url):
-    source_code = requests.get(book_url)
-    plain_text = source_code.text
-    soup = BeautifulSoup(plain_text,'html.parser')
-
-
-    for book in soup.findAll('article',{'class':'product_page'}):
-        # print(book.prettify())
-
-        title = book.h1.string
-        # print(title)                                          #title
-
-        price = soup.findAll('p')[0].string
-        # print(price)                                        #price
-
-        description = soup.findAll('p')[3].string
-        # print(description)                    #desription
-
-        UPC = soup.findAll('td')[0].string
-        # print(UPC)                   # UPC
-
-        stocks = soup.findAll('td')[5].string
-        # print(stocks)                   # stocks available
-
-        ratings = book.findAll('p')[2].get('class')[1]
-        # print(ratings)           # ratings
+    while page <= noOfPages :
+        t0 = time.time()
+        url = 'https://books.toscrape.com/catalogue/page-' + str(page) + '.html'
+        sourceCode = requests.get(url)
+        plainText = sourceCode.text
+        soup = BeautifulSoup(plainText, 'html.parser')
+        #print(soup.prettify())
+        for links in soup.find_all('article', {'class':'product_pod'}):
+            #print(links)
+            bookLink = 'https://books.toscrape.com/catalogue/' + links.h3.a.get('href')
+            eachBook(bookLink)
+            # print(temp)
+            # title = links.h3.a.get('title')
+            # print(title)
+            # rating = links.p.get('class')
+            # print(rating[1])
+        t1 = time.time()
+        print("-------Page "+str(page) + " Done--------\n")
+        print(f"------Total time taken {round(t1-t0,2)} seconds")
+        page += 1
+# article class = "product_pod"  div = "image_container"
 
 
-    for get_genre in soup.findAll('div', {'class': 'page_inner'}):
-        genre=soup.findAll('li')[2].a.string
-        # print(genre)                   # genre
-        break
+def eachBook(bookLink):
+    sourceCode = requests.get(bookLink)
+    plainText = sourceCode.text
+    bookSoup = BeautifulSoup(plainText, 'html.parser')
 
-    conn = pymysql.connect(host="localhost", user="root", passwd="", db="mypydb")
-    myCursor = conn.cursor()
-    args=(title,ratings,price,stocks,description,genre,UPC)
-    query = "INSERT INTO test1 VALUES (NULL, %s, %s, %s, %s, %s, %s, %s)"
-    myCursor.execute(query,args)
-    myCursor.close()
-    conn.commit()
-    conn.close()
+    title = bookSoup.h1.string
+    print(title)
+    price = bookSoup.p.string
+    rating = bookSoup.find_all('p')[2].get('class')[1]
+    description = bookSoup.find_all('p')[3].string
+    availibilty = bookSoup.find_all('td')[5].string
+    upc = bookSoup.find_all('td')[0].string
+    genre = bookSoup.find_all('li')[2].a.string
+    formatting(title, rating, price, availibilty, description, genre, upc)
 
-    print("done")
+def formatting(title, rating, price, availibilty, description, genre, upc):
+    title = title
+    rating_dict = {'One': '1', 'Two': '2', 'Three': '3', 'Four': '4', 'Five': '5'}
+    rating = int(rating_dict[rating])
+    description=description
+    price=float(price[2:])
+    availibilty = int(availibilty[10:-11])
+    upc = upc
+    genre = genre
+    insertingINdB(title, rating, price, availibilty, description, genre, upc)
 
-db.connect()
-trade_spider(3)
+def insertingINdB(title, rating, price, availibilty, description, genre, upc):
+    con = pymysql.connect(host='localhost', user='root', passwd='', db='scarpedb')
+    mycursor = con.cursor()
+    args=(title, rating, price, availibilty, description, genre, upc)
+    query="INSERT INTO books1 values(NULL, %s,  %s, %s, %s, %s, %s, %s)"
+    mycursor.execute(query, args)
+    con.commit()
+    con.close()
+
+fb.konnect()
+tradeSpider()
+
